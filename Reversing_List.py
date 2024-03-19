747eeaa559d0b1c8e58f1ec2c2a6caa220157f33
RamSainath1989 <sainath590221@gmail.com>
2024-03-19 13:01:34 -0500

    ReversingList

---
+++ Reversing_List.py
@@ -0,0 +1,14 @@
+# lst = [10, 11, 12, 13, 14, 15]
+# lst.reverse()
+# print("Using reverse() ", lst)
+
+# print("Using reversed() ", list(reversed(lst)))
+
+# Reversing a list using slicing technique
+def Reverse(lst):
+    new_lst = lst[::-1]
+    return new_lst
+
+
+lst = [10, 11, 12, 13, 14, 15]
+print(Reverse(lst))
